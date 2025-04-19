e1de2ebbe427b4461fb73992f7a912d3d4c7ad87
joa1gg <j.gabriel.gomes2976@gmail.com>
2025-04-19 08:01:43 -0400

    removed something not important

--- src/services/ytdlpapi.service.ts
+++ src/services/ytdlpapi.service.ts
@@ -2,9 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: "https://yt-dlp-api-production-19f7.up.railway.app/",
-    params: {
-        api_token: 'f748ab61-5e27-4df0-ba92-4a39d48dd2e4'
-    }
 })
 
 export const getInfoVideo = async (url: string) => {
